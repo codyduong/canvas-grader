46de2996b39156ffc0f1054a1e1baa667662b096
Cody Duong <duongcody507@gmail.com>
2022-03-12 10:04:58 -0600

    Add development server

--- craco.config.js
+++ craco.config.js
@@ -1,21 +1,29 @@
 module.exports = {
   webpack: {
-      configure: (webpackConfig, {env, paths}) => {
-          return {
-              ...webpackConfig,
-              entry: {
-                  main: [env === 'development' && require.resolve('react-dev-utils/webpackHotDevClient'),paths.appIndexJs].filter(Boolean),
-                  content: './src/chromeServices/DOM.ts',
-              },
-              output: {
-                  ...webpackConfig.output,
-                  filename: 'static/js/[name].js',
-              },
-              optimization: {
-                  ...webpackConfig.optimization,
-                  runtimeChunk: false,
-              }
-          }
-      },
-  }
-}
+    configure: (webpackConfig, { env, paths }) => {
+      return {
+        ...webpackConfig,
+        entry: {
+          main: [
+            env === 'development' &&
+              require.resolve('react-dev-utils/webpackHotDevClient'),
+            paths.appIndexJs,
+          ].filter(Boolean),
+          content: './src/chromeServices/DOM.ts',
+        },
+        output: {
+          ...webpackConfig.output,
+          filename: 'static/js/[name].js',
+        },
+        optimization: {
+          ...webpackConfig.optimization,
+          runtimeChunk: false,
+        },
+      };
+    },
+  },
+  devServer: (devServerConfig, { env, paths, proxy, allowedHost }) => {
+    devServerConfig.devMiddleware.writeToDisk = true;
+    return devServerConfig;
+  },
+};
